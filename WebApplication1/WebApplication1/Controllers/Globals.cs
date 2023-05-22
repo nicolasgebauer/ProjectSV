bf33837e5e0b3953e08019db67c518a2ca51e878
nicolasgebauer <ngebauer@miuandes.cl>
2023-05-22 15:31:18 -0400

    refactor Globals.cs

--- WebApplication1/WebApplication1/Controllers/Globals.cs
+++ WebApplication1/WebApplication1/Controllers/Globals.cs
@@ -1,9 +1,17 @@
-public static class Globals
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Controllers
 {
-    public const int MaxPercentage = 100;
-    public const string RegularizacionPatrimonioKey = "Regularización de Patrimonio";
-    public const string AcquirersUsersKey = "acquirers_users";
-    public const string AlienatorsUsersKey = "alienators_users";
-    public const int OldestYearKey = 2019;
-    public const int EmptyKey = 0;
+    public class Globals
+    {
+        public const int MaxPercentage = 100;
+        public const string RegularizacionPatrimonioKey = "Regularización de Patrimonio";
+        public const string AcquirersUsersKey = "acquirers_users";
+        public const string AlienatorsUsersKey = "alienators_users";
+        public const int OldestYearKey = 2019;
+        public const int EmptyKey = 0;
+    }
 }
